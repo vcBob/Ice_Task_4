546bab407156d903d8ef58967cd9f77289696d79
Bandile <ST10480829@vcconnect.edu.ze>
2025-05-02 21:15:55 +0200

    Score Summary Complte

--- app/src/main/java/com/example/icetask4/ScoreSummaryScreen.kt
+++ app/src/main/java/com/example/icetask4/ScoreSummaryScreen.kt
@@ -1,6 +1,8 @@
 package com.example.icetask4
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -17,9 +19,24 @@ class ScoreSummaryScreen : AppCompatActivity() {
         //call components from xml folder
        val customFeedback = findViewById<TextView>(R.id.txt_CustomFeedback)
         val txtScore = findViewById<TextView>(R.id.txt_score)
+        val txtReview = findViewById<TextView>(R.id.txt_Review)
         val reviewQuiz =findViewById<Button>(R.id.btn_ReviewQuiz)
         val btnExit =findViewById<Button>(R.id.btn_Exit)
+        val btnHideReview = findViewById<Button>(R.id.btn_HideReview)
         //==========================================================================================
 
+
+        btnExit.setOnClickListener{finishAffinity()}
+        reviewQuiz.setOnClickListener{
+            txtReview.visibility = View.VISIBLE
+            reviewQuiz.visibility= View.INVISIBLE
+            btnHideReview.visibility= View.VISIBLE
+
+            txtReview.text = "(1).Harry Potter has less than 1 million fans world wide. false (2).Pirates of the Caribbean's main protaganist is Jack Sparrow. true (3).IT was first releast in cinimas in 2024. false (4).The Minecraft movie is not about the video game mine craft. false (5). Stan Lee is the man behind the creation of the super-hero Spider-Man. true"
+        }
+        btnHideReview.setOnClickListener{
+            txtReview.visibility = View.INVISIBLE
+            reviewQuiz.visibility= View.VISIBLE
+            btnHideReview.visibility= View.INVISIBLE}
     }
 }
