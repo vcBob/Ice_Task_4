0ea7e5660e9e897d9c11fec1a632a664f9a3a94d
Bandile <ST10480829@vcconnect.edu.ze>
2025-05-02 15:26:56 +0200

    TriviaQuestionScreen and ScoreSummaryScreen components called to kt folder.

--- app/src/main/java/com/example/icetask4/ScoreSummaryScreen.kt
+++ app/src/main/java/com/example/icetask4/ScoreSummaryScreen.kt
@@ -8,19 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TriviaQuestionScreen : AppCompatActivity() {
+class ScoreSummaryScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.trivia_question_screen)
-
+        setContentView(R.layout.score_summary_screen)
         //==========================================================================================
-        //components called from the xml folder.
-        val txtQuestion = findViewById<TextView>(R.id.txt_Question)
-        val txtFeedback = findViewById<TextView>(R.id.txt_Feedback)
-        val btnTrue = findViewById<Button>(R.id.btn_True)
-        val btnFalse = findViewById<Button>(R.id.btn_False)
-        val btnNext = findViewById<Button>(R.id.btn_Next)
+        //call components from xml folder
+       val customFeedback = findViewById<TextView>(R.id.txt_CustomFeedback)
+        val txtScore = findViewById<TextView>(R.id.txt_score)
+        val reviewQuiz =findViewById<Button>(R.id.btn_ReviewQuiz)
+        val btnExit =findViewById<Button>(R.id.btn_Exit)
         //==========================================================================================
 
     }
